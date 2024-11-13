c0d5c9541762d14d88f16e4c953304f4169791c2
arshpsps <arshh.gill@outlook.com>
2024-11-13 16:44:27 +0530

    match first arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut args: std::env::Args = std::env::args();
+
+    let action: String = if let Some(x) = args.nth(1) {
+        match x.as_str() {
+            "uwu" => String::from("uwu"),
+            _ => String::from("uwu"),
+        }
+    } else {
+        String::from("sad")
+    };
+
+    println!("{}", action);
 }
